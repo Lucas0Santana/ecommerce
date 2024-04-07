1c0549b06c1e8a858cea7318cc55f29d41063801
Lucas Santana <lucas1.stoly@gmail.com>
2024-04-07 15:58:59 -0300

    corrigindo atributo dee visibilidade

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,23 +1,19 @@
+using System.Text.Json.Serialization;
 using ecommerce.Models;
 
-public class Cliente
+public class Cliente(string CPF, int idCredenciais)
 {
-    public Cliente()
-    {
-    }
+    public string CPF { get; private set; } = CPF;
+    public int IdCredenciais { get; private set; } = idCredenciais;
 
-    public Cliente(string cpf, int idCredenciais)
-    {
-        this.CPF = cpf;
-        this.IdCredenciais = idCredenciais;
-    }
-
-    public string CPF { get; private set; }
-    public int IdCredenciais { get; private set; }
-
+    [JsonIgnore]
     public Credenciais? Credenciais { get; set; }
+    [JsonIgnore]
     public ICollection<Endereco> Enderecos { get; set; } = new List<Endereco>();
+    [JsonIgnore]
     public ICollection<Pedido>? Pedidos { get; set; }
+    [JsonIgnore]
     public Carrinho? Carrinho { get; set; }
+    [JsonIgnore]
     public ICollection<SolicitacaoDeEstorno>? SolicitacaoDeEstornos { get; set; }
 }
